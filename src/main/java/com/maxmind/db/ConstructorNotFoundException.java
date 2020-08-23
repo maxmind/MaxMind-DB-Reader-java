c7b4d388f8f1d33d8afa6b0953bb14061c29bc13
William Storey <wstorey@maxmind.com>
2020-08-23 10:58:35 -0700

    Make new exceptions runtime exceptions

--- src/main/java/com/maxmind/db/ConstructorNotFoundException.java
+++ src/main/java/com/maxmind/db/ConstructorNotFoundException.java
@@ -1,12 +1,10 @@
 package com.maxmind.db;
 
-import java.io.IOException;
-
 /**
  * Signals that no annotated constructor was found. You should annotate a
  * constructor in the class with the MaxMindDbConstructor annotation.
  */
-public class ConstructorNotFoundException extends IOException {
+public class ConstructorNotFoundException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
     ConstructorNotFoundException(String message) {
