910a4b39361d8a43b3443feae90bc8b6b6702db2
Gregory Oschwald <goschwald@maxmind.com>
2020-08-27 13:30:15 -0700

    Update class name in docs

--- src/main/java/com/maxmind/db/Reader.java
+++ src/main/java/com/maxmind/db/Reader.java
@@ -154,7 +154,7 @@ public final class Reader implements Closeable {
      *
      * @param ipAddress the IP address to look up.
      * @return the record for the IP address. If there is no data for the
-     * address, the non-null {@link Record} will still be returned.
+     * address, the non-null {@link DatabaseRecord} will still be returned.
      * @throws IOException if a file I/O error occurs.
      */
     public <T> DatabaseRecord<T> getRecord(InetAddress ipAddress, Class<T> cls)
