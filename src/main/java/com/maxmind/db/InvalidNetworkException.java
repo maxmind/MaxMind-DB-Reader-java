5cca672e5c674556da87ccb22f4e3e1f9143a183
Shadi Romani <sromani@maxmind.com>
2024-01-22 17:25:33 +0000

    Remove condition on the minimal amount of lines in method

--- src/main/java/com/maxmind/db/InvalidNetworkException.java
+++ src/main/java/com/maxmind/db/InvalidNetworkException.java
@@ -7,6 +7,10 @@ import java.net.InetAddress;
  * IPv6 address in an IPv4-only database.
  */
 public class InvalidNetworkException extends Exception {
+    /**
+     * @param ip
+     *            the IP address that was used
+     */
     public InvalidNetworkException(InetAddress ip) {
         super("you attempted to use an IPv6 network in an IPv4-only database: " + ip.toString());
     }
--- src/main/java/com/maxmind/db/NodeCache.java
+++ src/main/java/com/maxmind/db/NodeCache.java
@@ -10,9 +10,28 @@ public interface NodeCache {
      * A loader is used to load a value for a key that is not in the cache.
      */
     interface Loader {
+        /**
+         * @param key
+         *            the key to load
+         * @return the value for the key
+         * @throws IOException
+         *             if there is an error loading the value
+         */
         DecodedValue load(CacheKey key) throws IOException;
     }
 
+    /**
+     * This method returns the value for the key. If the key is not in the cache
+     * then the loader is called to load the value. 
+     *
+     * @param key
+     *            the key to look up
+     * @param loader
+     *            the loader to use if the key is not in the cache
+     * @return the value for the key
+     * @throws IOException
+     *             if there is an error loading the value
+     */
     DecodedValue get(CacheKey key, Loader loader) throws IOException;
 
 }
